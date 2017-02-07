8b88b0fce5e3d78c562474076f607b3e09ac5993
VERSAYANA <versayanaa@gmail.com>
2017-02-07 04:42:11 +0330

    commit

--- src/components/TodosComponent.js
+++ src/components/TodosComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button, View, Text, Switch, StyleSheet, TextInput } from 'react-native';
-import CheckBox from 'react-native-icon-checkbox';
+import CheckBox from 'react-native-checkbox';
 
 export default class TodosComponent extends React.Component {
   static navigationOptions = {
@@ -11,38 +11,60 @@ export default class TodosComponent extends React.Component {
     const { todos, filter, toggleFilter, complete, addTodo } = this.props;
     const { title } = this.props.navigation.state.params
     return (
-      <View>
-        <TextInput
-          style={s.input}
-          placeholder="Add New Todo"
-          placeholderTextColor="white"
-          ref={title}
-          onSubmitEditing={(v) => {addTodo(v.nativeEvent.text, title);
-            this.refs[title].setNativeProps({text: ''});
-            this.refs[title].blur();
-          }}
-        />
+      <View style={{flex:1, justifyContent: 'space-between'}}>
+
+        <View>
+        <View style={s.switchContainer}>
+          <Text style={{marginLeft: 8}}>Show Completed Todos</Text>
         <Switch
+          style={{marginRight: 8}}
           value={filter}
           onValueChange={() => {toggleFilter()}}
         />
+      </View>
+
+
         {todos.map((x,i) =>
-          <CheckBox
-            key={i}
-            label={x.text}
-            color="red"
-            iconStyle={{color: 'blue'}}
-            checked={x.completed}
-            onPress={a => complete(x.id)}
-            labelStyle={{textDecorationLine: x.completed ? 'line-through' : 'none'}}
-          />
+          // <View >
+            <CheckBox
+              containerStyle={s.itemContainer}
+              key={i}
+              label={x.text}
+              checked={x.completed}
+              labelStyle={{textDecorationLine: x.completed ? 'line-through' : 'none'}}
+              onChange={() => {complete(x.id)}}
+              checkboxStyle={{height: 20, width: 20}}
+            />
+          // </View>
       )}
+    </View>
+
+      <TextInput
+        style={s.input}
+        placeholder="Add New Todo"
+        placeholderTextColor="white"
+        ref={title}
+        onSubmitEditing={(v) => {addTodo(v.nativeEvent.text, title);
+          this.refs[title].setNativeProps({text: ''});
+          this.refs[title].blur();
+        }}
+      />
       </View>
     )
   }
 }
 
 const s = StyleSheet.create({
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    borderBottomColor: '#E0E0E0',
+    height: 50,
+    // justifyContent: 'center',
+    borderBottomWidth: 1,
+  },
   input: {
     height: 50,
     backgroundColor: '#F4511E',
@@ -50,4 +72,14 @@ const s = StyleSheet.create({
     paddingRight: 15,
     color: 'white',
   },
+  itemContainer: {
+    backgroundColor: 'white',
+    borderBottomColor: '#E0E0E0',
+    height: 50,
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    marginBottom: 0,
+    paddingLeft: 8,
+    justifyContent: 'flex-start',
+  }
 });
