3e596f9bf436a1512e56d94d3a1f96c17398a6a3
Ramtin <kh.ramtin@gmail.com>
2017-03-20 02:26:20 +0330

    start

--- index.android.js
+++ index.android.js
@@ -1,13 +1,32 @@
 
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from './src/App';
 import reducer from './src/reducers';
 import devToolsEnhancer from 'remote-redux-devtools';
 
-const store = createStore(reducer, devToolsEnhancer());
+import { createStore } from "redux";
+import { ApolloClient, ApolloProvider } from 'react-apollo';
+
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj0glz8cz8soy013606fq2z0z' }),
+});
+
+const store = createStore(
+  combineReducers({
+    todos: todoReducer,
+    users: userReducer,
+    apollo: client.reducer(),
+  }),
+  {}, // initial state
+  compose(
+      applyMiddleware(client.middleware()),
+      // If you are using the devToolsExtension, you can add it here also
+      (typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+  )
+);
 
 
 const NativeTodo = () => {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from "redux";
-import todos from "./todos";
-import lists from "./lists";
+// import todos from "./todos";
+// import lists from "./lists";
 import filter from "./filter";
 
 const index = combineReducers({
-  todos,
-  lists,
+  // todos,
+  // lists,
   filter
 });
 
